c259fcd83fc4e24c14da46d45637fa12b1076ada
sajidrahii <sajidahmedrahii@gmail.com>
2026-03-13 23:52:19 +0600

    active member count js

---
+++ script.js
@@ -0,0 +1,9 @@
+let proact_members = 20;
+let act_members = 27;
+let general_members = 56;
+
+document.getElementById("proact_member_count").innerHTML = proact_members;
+
+document.getElementById("act_member_count").innerHTML = act_members + "+";
+
+document.getElementById("gen_member_count").innerHTML = general_members + "+";

f1f0f4de04bd20cc832f15b47dfcffdf69848995
sajidrahii <sajidahmedrahii@gmail.com>
2026-03-14 21:24:49 +0600

    members updated

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-let proact_members = 20;
-let act_members = 27;
-let general_members = 56;
+let proact_members = 22;
+let act_members = 30;
+let general_members = 60;
 
 document.getElementById("proact_member_count").innerHTML = proact_members;
 
 document.getElementById("act_member_count").innerHTML = act_members + "+";
 
 document.getElementById("gen_member_count").innerHTML = general_members + "+";
+
+
+
+
+
+// FILE PATH FOR ATTACHMENTS
